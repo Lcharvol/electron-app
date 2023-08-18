b875d3c34bf402965c577b1f55ab65b3361b8438
Lucas CHARVOLIN <charvolin0@gmail.com>
2023-08-18 15:54:38 +0200

    feat: add Tabs storie

---
+++ src/renderer/UI/Tabs/Tabs.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Tabs from '.';
+import Typography from '../Typography';
+import Box from '../Box';
+
+const meta: Meta<typeof Tabs> = {
+  component: Tabs,
+};
+
+type Story = StoryObj<typeof Tabs>;
+
+export const Basic: Story = {
+  args: {
+    items: [
+      {
+        label: 'Tab1',
+        component: () => (
+          <Box sx={{ p: 2 }}>
+            <Typography>Tab1</Typography>
+          </Box>
+        ),
+      },
+      {
+        label: 'Tab2',
+        component: () => (
+          <Box sx={{ p: 2 }}>
+            <Typography>Tab2</Typography>
+          </Box>
+        ),
+      },
+      {
+        label: 'Tab3',
+        component: () => (
+          <Box sx={{ p: 2 }}>
+            <Typography>Tab3</Typography>
+          </Box>
+        ),
+      },
+    ],
+  },
+  parameters: {
+    controls: { include: ['items'] },
+  },
+};
+
+export default meta;
